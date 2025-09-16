0db449d98965bcf14e43f83cb4f0286d78252fd6
Juan Manuel Perez Luciano <jm@192.168.1.74>
2025-09-15 18:07:10 -0600

    update for formspark

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/static'; // 👈 adapter estático para Vercel
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',       // 👈 importante para Vercel
+  adapter: vercel(),      // 👈 activa el adapter
   vite: {
     plugins: [tailwindcss()],
     resolve: {
